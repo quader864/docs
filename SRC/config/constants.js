8e49935bd4b5637ec812b306ebe46683992b45e9
aliquader <alihackerreza123@gmail.com>
2025-12-02 14:53:55 +0330

    deleted server files

--- SRC/config/constants.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  FRONTEND_URL: process.env.FRONTEND_ORIGIN || "https://quader864.github.io",
-  JWT_SECRET: process.env.JWT_SECRET || "supersecretjwtkey",
-  PORT: process.env.PORT || 3000
-};
--- SRC/models/MT5_Orders.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const MT5_Orders = new mongoose.Schema({
-  accountNumber: String,
-  orders: Array}, {
-  timestamps: true  
-});
-
-module.exports = mongoose.model("MT5_Orders", MT5_Orders);
